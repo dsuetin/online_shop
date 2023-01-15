d47178e96dcb753388d10870965839a07f95b20d
dsuetin <suetindaniil@gmail.com>
2023-01-15 15:06:36 +0300

    add appRouter

---
+++ client/src/components/AppRouther.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Routes, Route, Redirect} from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+        <div>
+            WOrkng
+        </div>
+    );
+};
+
+export default AppRouter;
