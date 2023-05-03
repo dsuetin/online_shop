5dea6deb5e6769ab658c0d76724d59fbe969c3ee
dsuetin <suetindaniil@gmail.com>
2023-05-03 14:11:10 +0300

    inline block in brandbar

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -8,21 +8,18 @@ const BrandBar = observer(() => {
   // console.log('device', device.getBrands()[0].id, device.getBrands()[0].name);
   // console.log('jjjj', device.getSelectedBrand());
   return (
-    <Form className="d-flex">
+    <Form className="w-100">
       {(device.getBrands().map((brand) => (
         <Card
           md={4}
           style={{ cursor: 'pointer' }}
           key={brand.id}
-          className="p-3 mt-2"
+          className="p-3 mt-2 d-inline-block"
           onClick={() => device.setSelectedBrand(brand)}
-          active={brand.id === device.getSelectedBrand().id}
-          // border={console.log('dddddddddd', device.getSelectedBrand().id)}
           border={brand.id === device.getSelectedBrand().id ? 'danger' : 'light'}
         >
           { brand.name }
         </Card>
-        // 'lalal'
       )))}
     </Form>
   );
