546f99c898f99cb15f806fc81a4ad3d9717bc730
dsuetin <suetindaniil@gmail.com>
2023-05-14 12:02:53 +0300

    update admin page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
 import CreateType from '../components/modals/CreateType';
 
-const Admin = () => (
-  <Container
-    className="d-flex flex-column"
-  >
-    <Button
-      variant="outline-dark"
-      className="mt-4 p-2"
-    >
-      Добавить тип
-    </Button>
-    <Button
-      variant="outline-dark"
-      className="mt-4 p-2"
-    >
-      Добавить бренд
-    </Button>
-    <Button
-      variant="outline-dark"
-      className="mt-4 p-2"
-    >
-      Добавить устройство
-    </Button>
-    <CreateType show />
-  </Container>
-);
+const Admin = () => {
+  const [typeVisible, setTypeVisible] = useState(false);
+  const [brandVisible, setBrandVisible] = useState(false);
+  const [diviceVisible, setDeviceVisible] = useState(false);
+
+  return (
+    <Container className="d-flex flex-column">
+      <Button
+        variant="outline-dark"
+        className="mt-4 p-2"
+        onClick={() => setTypeVisible(true)}
+      >
+        Добавить тип
+      </Button>
+      <Button
+        variant="outline-dark"
+        className="mt-4 p-2"
+        onClick={() => setBrandVisible(true)}
+      >
+        Добавить бренд
+      </Button>
+      <Button
+        variant="outline-dark"
+        className="mt-4 p-2"
+        onClick={() => setDeviceVisible(true)}
+      >
+        Добавить устройство
+      </Button>
+      <CreateType show={typeVisible} onHide={() => setTypeVisible(false)} />
+      <CreateType show={brandVisible} onHide={() => setBrandVisible(false)} />
+      <CreateType show={diviceVisible} onHide={() => setDeviceVisible(false)} />
+    </Container>
+  );
+};
+
 export default Admin;
