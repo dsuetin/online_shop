9874ba31aa8227574975c13c0c8594e08792ef2d
dsuetin <suetindaniil@gmail.com>
2023-05-21 15:21:25 +0300

    chaende to return

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -41,57 +41,58 @@ const click = async () => {
 // const [password, setPassword] = useState('');
 
 // const [diviceVisible, setDeviceVisible] = useState(false);
+const Auth = () => {
+  const [brandVisible, setBrandVisible] = useState(false);
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ heigth: window.innerHeight - 540 }}
+      // style={{ heigth: '1000vh' }}
+    >
+      <Card style={{ outerWidth: 600 }} className="p-5">
+        <h2 className="m-auto">Авторизация</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш email"
+            // value={email}
+            // onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш пароль"
+            // value={password}
+            // onChange={(e) => setPassword(e.target.value)}
+            // type="password"
+          />
+          <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+            {isLogin()
+              ? (
+                <div>
+                  Нет аккаунта?
+                  <NavLink to={REGISTRATION_ROUTE}> Зарегистрируйся!</NavLink>
+                </div>
+              )
+              : (
+                <div>
+                  Есть аккаунт?
+                  {' '}
+                  <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                </div>
+              )}
+            <Button
+              variant="outline-success"
+              className="mt-3 align-self-end"
+              // onClick={click}
+            >
+              {isLogin() ? 'Войти' : 'Регистрация'}
+            </Button>
+          </Row>
 
-const Auth = () => (
-  // const [brandVisible, setBrandVisible] = useState(false);
-  <Container
-    className="d-flex justify-content-center align-items-center"
-    style={{ heigth: window.innerHeight - 540 }}
-    // style={{ heigth: '1000vh' }}
-  >
-    <Card style={{ outerWidth: 600 }} className="p-5">
-      <h2 className="m-auto">Авторизация</h2>
-      <Form className="d-flex flex-column">
-        <Form.Control
-          className="mt-3"
-          placeholder="Введите ваш email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
-        />
-        <Form.Control
-          className="mt-3"
-          placeholder="Введите ваш пароль"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
-          // type="password"
-        />
-        <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
-          {isLogin()
-            ? (
-              <div>
-                Нет аккаунта?
-                <NavLink to={REGISTRATION_ROUTE}> Зарегистрируйся!</NavLink>
-              </div>
-            )
-            : (
-              <div>
-                Есть аккаунт?
-                {' '}
-                <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
-              </div>
-            )}
-          <Button
-            variant="outline-success"
-            className="mt-3 align-self-end"
-            // onClick={click}
-          >
-            {isLogin() ? 'Войти' : 'Регистрация'}
-          </Button>
-        </Row>
-
-      </Form>
-    </Card>
-  </Container>
-);
+        </Form>
+      </Card>
+    </Container>
+  );
+};
 
 export default Auth;
