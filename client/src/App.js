e3b5d161a32d56e7efb21a907efad5fd540447be
dsuetin <suetindaniil@gmail.com>
2023-05-23 23:39:49 +0300

    change function to const

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component, useContext } from 'react';
+import { observer } from 'mobx-react-lite';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouther from './components/AppRouther';
 import NavBar from './components/NavBar';
+import { Context } from './index';
 
-function App() {
+const App = observer(() => {
+  const { user } = useContext(Context);
+  console.log('aaauuuthhhhhh', user);
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const navigation = useNavigate();
+  // const location = () => {
+  //   const { pathname } = useLocation();
+  //   return (pathname);
+  // };
+  // const isLogin = () => (location() === LOGIN_ROUTE);
   return (
     <Router>
       <NavBar />
       <AppRouther />
     </Router>
-  );
-}
 
+  );
+});
 export default App;
