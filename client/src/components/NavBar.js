69349762eaf2cc387e3c741755dc55292913e88b
dsuetin <suetindaniil@gmail.com>
2023-05-26 22:39:02 +0300

    change navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { NavLink, useNavigate } from 'react-router-dom';
 // import Shop from "../pages/Shop";
-import { Button } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
 import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 
@@ -16,58 +16,82 @@ const NavBar = observer(() => {
   console.log(user);
   console.log(user.getIsAuth());
   const navigation = useNavigate();
+
+  const logOut = () => {
+    user.setUser({});
+    user.setIsAuth(false);
+  };
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand>Gedonism</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          {user.getIsAuth()
-            ? (
-              <Nav
-                className="mr-auto"
-                style={{ color: 'white' }}
-              >
-                <Button
-                  variant="outline-light"
-                  onClick={() => navigation(ADMIN_ROUTE)}
-                >
-                  Admin panel
-                </Button>
-                <div
-                  className="col-sm-1 col-xs-1 col-md-1 col-lg-1"
-                >
-                  { }
-                </div>
-                <Button
-                  variant="outline-light"
-                  className="p-xl-4"
-                  onClick={() => navigation(LOGIN_ROUTE)}
-                >
-                  Exit
-                </Button>
-              </Nav>
-            )
-            : (
-              <Nav
-                className="m-lg-auto"
-                style={{ color: 'white' }}
-              >
-                <Button
-                  variant="outline-light"
-                  onClick={() => user.setIsAuth(true)}
-                >
-                  Authorization
-                </Button>
-              </Nav>
-            )}
-        </Navbar.Collapse>
+        {/* <Navbar.Brand>Gedonism</Navbar.Brand> */}
+        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
         <NavLink
           style={{ color: 'white' }}
           to={SHOP_ROUTE}
         >
           Bike Shop
         </NavLink>
+
+        {user.getIsAuth()
+          ? (
+            <Nav
+              className="ml-auto"
+              style={{ color: 'white' }}
+            >
+              <Col>
+                <Button
+                  variant="outline-light"
+                  onClick={() => navigation(ADMIN_ROUTE)}
+                >
+                  Admin panel
+                </Button>
+                {' '}
+                <Button
+                  variant="outline-light"
+                  onClick={() => logOut()}
+                  className="ml-2"
+                >
+                  Exit
+                </Button>
+              </Col>
+
+              {/* <div
+                className="
+                // col-lg-1
+                "
+              >
+                {' '}
+              </div> */}
+              {/* <div
+                className="
+                // col-sm-1
+                // col-xs-1
+                // col-md-1
+                // col-lg-1
+                "
+              >
+                {' '}
+              </div> */}
+
+            </Nav>
+          )
+          : (
+            <Nav
+              className="ml-auto"
+              style={{ color: 'white' }}
+            >
+              <Button
+                variant="outline-light"
+                onClick={() => navigation(LOGIN_ROUTE)}
+              >
+                Authorization
+              </Button>
+            </Nav>
+          )}
+        {/* <Navbar.Collapse id="responsive-navbar-nav">
+
+        </Navbar.Collapse> */}
       </Container>
     </Navbar>
   );
