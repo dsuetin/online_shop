40d2b65844ca8900ab032d098404e7c87d8f938b
dsuetin <suetindaniil@gmail.com>
2023-05-27 00:25:59 +0300

    add devices

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -7,7 +7,37 @@ export const CreateType = async (type) => {
 };
 
 export const fetchTypes = async () => {
-  const { data } = await $host.post('api/type');
+  const { data } = await $host.get('api/type');
   console.log('fetchTypes', data);
+  return data;
+};
+
+export const CreateBrand = async (brand) => {
+  const { data } = await $authHost.post('api/brand', { brand });
+  console.log('CreateBrand', data);
+  return data;
+};
+
+export const fetchBrands = async () => {
+  const { data } = await $host.get('api/brand');
+  console.log('fetchBrands', data);
+  return data;
+};
+
+export const CreateDevice = async (device) => {
+  const { data } = await $authHost.post('api/device', { device });
+  console.log('CreateDevice', data);
+  return data;
+};
+
+export const fetchDevices = async () => {
+  const { data } = await $host.get('api/device');
+  console.log('fetchDevices', data);
+  return data;
+};
+
+export const fetchOneDevice = async (DeviceId) => {
+  const { data } = await $host.get(`api/device${DeviceId}`);
+  console.log('fetchDevices', data);
   return data;
 };
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Form, Container, Col, Row } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
 import TypeBar from '../components/TypeBar';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
+import { Context } from '../index';
+import { fetchTypes, fetchBrands, fetchDevices } from '../http/deviceAPI';
 
 // import DeviceList from "../components/DeviceList";
 
 const Shop = observer(() => {
+  const { device } = useContext(Context);
+  useEffect(() => {
+    fetchTypes().then((typesData) => { return device.setTypes(typesData); });
+    fetchBrands().then((brandsData) => { return device.setBrands(brandsData); });
+    fetchDevices().then((devicesData) => { return device.setDeivces(devicesData.rows); });
+  }, []);
   return (
     <Container>
       <Row className="mt-2">
