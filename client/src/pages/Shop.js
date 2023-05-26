645c252353cde39a7c3d3f5016719ae06a7e9da6
dsuetin <suetindaniil@gmail.com>
2023-05-26 23:41:25 +0300

    add observer to shop

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Form, Container, Col, Row } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 import TypeBar from '../components/TypeBar';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
+
 // import DeviceList from "../components/DeviceList";
 
-const Shop = () => (
-  <Container>
-    <Row className="mt-2">
-      <Col md={3}>
-        <TypeBar />
-      </Col>
-      <Col md={9} className="mt-2">
-        <BrandBar />
-        <DeviceList />
-      </Col>
-    </Row>
-  </Container>
-);
+const Shop = observer(() => {
+  return (
+    <Container>
+      <Row className="mt-2">
+        <Col md={3}>
+          <TypeBar />
+        </Col>
+        <Col md={9} className="mt-2">
+          <BrandBar />
+          <DeviceList />
+        </Col>
+      </Row>
+    </Container>
+  );
+});
 export default Shop;
